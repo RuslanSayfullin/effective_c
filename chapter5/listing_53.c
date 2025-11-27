61d95c097dfac918aff29724ced67219a55fc824
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-11-27 23:08:09 +0500

    Обновлен проект

--- chapter5/listing_53.c
+++
@@ -1,9 +0,0 @@
-/* golf.c -- таблица результатов турнира no гольфу */
-#include <stdio.h>
-int main(void) {
-    int jane, tarzan, cheeta;
-    cheeta = tarzan = jane = 68;
-    printf ("           чита    тарзан      джейн\n") ;
-    printf("Счет первого раунда %4d %8d %8d\n", cheeta,tarzan, jane) ;
-    return 0;
-}
