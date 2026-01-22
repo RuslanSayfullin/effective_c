1173f5e535dc1f1e3b4c8b586310f0544a9ea5bd
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2026-01-22 19:28:58 +0500

    Map - это контейнер в STL, который используется для хранения элементов в виде пары ключ-значение. Внутренне элементы map всегда сортируются по ее ключу. Карты в основном реализованы в виде бинарных деревьев поиска.

---
+++ std_map_at.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // take any two maps
+    map<int, char> map1;
+    map<char, int> map2;
+
+    map[1] = 'a';
+    map[2] = 'b';
+    map[3] = 'c';
+    map[4] = 'd';
+
+    // Print the associated element
+    cout << "Element at map1[2] = " << map1.at(2) << endl;
+
+    cout << "Element at map2['w'] = " << map2.at('w') << endl;
+
+    return 0;
+}
